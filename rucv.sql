e2bb249ce913d343338a71ca20e5b996510d6f7c
Harry Wheildon <Harry Wheildon harry,wheildon@gmail.com>
2021-05-24 18:51:46 +0100

    Better filenames

--- rucv.sql
+++ rucv.sql
@@ -1,3 +1,3 @@
 CREATE TABLE speaker (id INTEGER PRIMARY KEY, cv_id TEXT UNIQUE, gender TEXT, total_upvotes INTEGER, total_downvotes INTEGER);
-CREATE TABLE sentence (id INTEGER PRIMARY KEY, content TEXT, selected INTEGER, score INTEGER, filename TEXT, crc INTEGER, speaker INTEGER, upvotes INTEGER, downvotes INTEGER, UNIQUE(crc, speaker));
+CREATE TABLE sentence (id INTEGER PRIMARY KEY, content TEXT, selected INTEGER, score INTEGER, original_filename TEXT, new_filename TEXT, crc INTEGER, speaker INTEGER, upvotes INTEGER, downvotes INTEGER, UNIQUE(crc, speaker));
 CREATE TABLE frequency (id INTEGER PRIMARY KEY, lemma TEXT, rank INTEGER, pos TEXT, frequency REAL);
